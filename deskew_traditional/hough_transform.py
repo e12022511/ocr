20f308672ee0e64adc097e73949812facad6e3d2
Philipp Gorke <e12022511@student.tuwien.ac.at>
2024-02-22 21:59:01 +0100

    #4
    Improved Hough Transformation by not taking median, but mean of most frequent angles in hough transform.

--- deskew_traditional/hough_transform.py
+++ deskew_traditional/hough_transform.py
@@ -1,4 +1,5 @@
-import numpy as np
+from collections import Counter
+
 import skimage.transform
 
 from deskew_traditional.deskew_method import DeskewMethod
@@ -6,13 +7,19 @@ from deskew_traditional.deskew_utils import *
 
 
 class HoughTransform(DeskewMethod):
+
     def deskew(self, image):
         sobel_image = apply_sobel(image)
-        theta_range = np.linspace(0, np.pi, 180)
+        theta_range = np.deg2rad(np.arange(0.1, 180.0))
         hspace, angles, distances = skimage.transform.hough_line(sobel_image, theta_range)
         _, angles, _ = skimage.transform.hough_line_peaks(hspace, angles, distances)
 
-        median_angle = np.median(angles)
-        rotation_angle = np.rad2deg(median_angle) - 90
-        print(rotation_angle)
-        return rotate_image(image, rotation_angle)
+        angles_in_degree = [np.rad2deg(angle) for angle in angles]
+
+        counter = Counter(angles_in_degree)
+        max_value = max(counter.values())
+        most_common_values = []
+        for value, count in counter.items():
+            if count == max_value:
+                most_common_values.append(value)
+        return np.mean(most_common_values) - 90
